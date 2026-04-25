acea355ce7642c12bedc2ca3afc4916a982f7c62
ketpatil77 <ket.patil77@gmail.com>
2026-04-25 04:20:05 +0000

    deploy fresh static portfolio (main)

--- app/src/components/ui/spline.tsx
+++
@@ -1,6 +0,0 @@
-export { SplineScene, SplineLoader, SplineErrorFallback, SplineSceneFallback } from './spline-scene';
-export type { SplineSceneProps } from './spline-scene';
-export { SplineWithFallback } from './spline-with-fallback';
-export { InteractiveSpline } from './interactive-spline';
-export { AccessibleSpline } from './accessible-spline';
-export { LazySpline } from './lazy-spline';
--- app/src/utils/3d.ts
+++
@@ -1,3 +0,0 @@
-export { checkWebGLSupport, checkWebGL2Support } from './webgl';
-export { getDeviceCapabilities, shouldLoad3DContent, getOptimalSceneQuality } from './device';
-export type { DeviceCapabilities, PerformanceLevel } from './device';
